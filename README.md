5993fc9df85a081a0affb90ce91794bed16719b1
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-11 13:36:29 +0700

    #1 - Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-boot-twilio-otp-sms-demo
---
+++ src/main/java/com/hendisantika/SpringBootTwilioOtpSmsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTwilioOtpSmsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTwilioOtpSmsDemoApplication.class, args);
+    }
+
+}
