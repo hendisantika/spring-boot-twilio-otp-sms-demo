f65cb3c7ebfb9fd22ad0b15b1511542fafbce7fe
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-11 13:44:03 +0700

    #5 - Add TwilioInitializer

---
+++ src/main/java/com/hendisantika/config/TwilioInitializer.java
@@ -0,0 +1,28 @@
+package com.hendisantika.config;
+
+import com.twilio.Twilio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-twilio-otp-sms-demo
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/03/22
+ * Time: 13.42
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class TwilioInitializer {
+
+    private final TwilioProperties twilioProperties;
+
+    @Autowired
+    public TwilioInitializer(Twilioproperties twilioProperties) {
+        this.twilioProperties = twilioProperties;
+        Twilio.init(twilioproperties.getAccountSid(), twilioproperties.getAuthToken());
+        System.out.println("Twilio initialized with account-" + twilioproperties.getAccountSid());
+    }
+}
