a7119fec6d54eaab3ae643018b0b9e46dfd8b878
Hendi Santika <hendisantika@yahoo.co.id>
2022-03-11 13:54:00 +0700

    #9 - Add startVerification in PhoneVerificationService

--- src/main/java/com/hendisantika/service/PhoneVerificationService.java
+++ src/main/java/com/hendisantika/service/PhoneVerificationService.java
@@ -1,6 +1,8 @@
 package com.hendisantika.service;
 
 import com.hendisantika.config.TwilioProperties;
+import com.twilio.exception.ApiException;
+import com.twilio.rest.verify.v2.service.Verification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +22,20 @@ public class PhoneVerificationService {
     private final TwilioProperties twilioProperties;
 
     @Autowired
-    public PhoneverificationService(TwilioProperties twilioProperties) {
+    public PhoneVerificationService(TwilioProperties twilioProperties) {
         this.twilioProperties = twilioProperties;
     }
+
+    public VerificationResult startVerification(String phone) {
+        try {
+            Verification verification = Verification.creator(twilioProperties.getServiceId(), phone, "sms").create();
+            if ("approved".equals(verification.getStatus()) || "pending".equals(verification.getStatus())) {
+                return new VerificationResult(verification.getSid());
+            }
+        } catch (ApiException exception) {
+            return new VerificationResult(new String[]{exception.getMessage()});
+        }
+        return null;
+    }
+
 }
